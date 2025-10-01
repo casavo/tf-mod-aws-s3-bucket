52f4339118f2bbd85e0bb59d3c50fde8d207fffc
Alessandro Lorenzi <me@alorenzi.eu>
2025-10-01 18:10:01 +0200

    feat: output bucket name

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "aws_s3_bucket_arn" {
     value = aws_s3_bucket.bucket.arn
     description = "The ARN of the S3 bucket."
 }
+
+output "name" {
+    value = aws_s3_bucket.bucket.bucket
+    description = "The name of the S3 bucket."
+}
