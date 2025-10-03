5678957eaf8ac382fa0053aa6f19e9ad4591d16e
Alessandro Lorenzi <me@alorenzi.eu>
2025-10-03 14:32:31 +0200

    chore: add domain name

--- output.tf
+++ output.tf
@@ -7,3 +7,8 @@ output "name" {
     value = aws_s3_bucket.bucket.bucket
     description = "The name of the S3 bucket."
 }
+
+output "bucket_domain_name" {
+    value = aws_s3_bucket.bucket.bucket_domain_name
+    description = "The domain name of the S3 bucket."
+}
